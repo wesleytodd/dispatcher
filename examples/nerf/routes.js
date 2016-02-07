dec7482c2f7379efe5d79ddc3de0578589586661
Wes Todd <wes@wesleytodd.com>
2016-02-07 10:42:02 -0600

    use shared-context in example

--- examples/nerf/routes.js
+++ examples/nerf/routes.js
@@ -1,6 +1,5 @@
 var reactExpressMiddleware = require('react-express-middleware');
-var escape = require('recursive-escape');
-var unescape = require('recursive-unescape');
+var sharedContext = require('shared-context');
 
 module.exports = function (app) {
 	// Isomorphic react render middleware
@@ -8,23 +7,8 @@ module.exports = function (app) {
 		element: 'app'
 	}));
 
-	// Setup a context to share with the browser
-	app.use(function setupContext (req, res, next) {
-		if (typeof window !== 'undefined' && window.__context && window.__context) {
-			// On the front-end we unescape it and null it our
-			res.locals.context = unescape(window.__context);
-			window.__context = null;
-		} else {
-			// On the backend or after the initial page load, we just initialize it
-			res.locals.context = {};
-		}
-
-		// Escape when outputing on page
-		res.locals.context.toJSON = function () {
-			return escape(res.locals.context);
-		};
-		next();
-	});
+	// Setup the context to share with the browser
+	app.use(sharedContext());
 
 	// Page routes
 	app.get('/', require('./handlers/index'));
